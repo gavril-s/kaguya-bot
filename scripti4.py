facf8818708cad6b99922a588bc06f681e978a81
gavril-s <gavril.senkevich@gmail.com>
2022-03-09 17:31:10 +0300

    emo

--- scripti4.py
+++ scripti4.py
@@ -1,3 +1,4 @@
+from tkinter import NE
 from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
 from telegram import ReplyKeyboardMarkup
 from glob import glob
@@ -6,6 +7,30 @@ from random import choice
 import random
 import datetime
 
+WORDS = dict()
+POSITIVE_REPLIES = ['Хорошо, сенпаи^^','Да, согласна)','Ты такой милашка','Ага)','Хорошо)', 'Desu-desu :3']
+NEGATIVE_REPLIES = ['Чеел','Иди нахуй','Эмм','Лол', 'Не пиши сюда больше, бака']
+
+def read_words():
+    f = open('words.txt', 'r')
+    for line in f:
+        tmp = line.split()
+        WORDS[tmp[0]] = int(tmp[1])
+    f.close()
+
+
+def compute_emo_rate(msg):
+    del_list = '.,;!?:`'
+    p_msg = msg.strip().lower()
+    trantab = p_msg.maketrans('', '', del_list)
+    p_msg = p_msg.translate(trantab)
+
+    msg_words = p_msg.split()
+    rate = 0
+    for word in msg_words:
+        if word in WORDS:
+            rate += WORDS[word]
+    return rate
 
 def sms(bot, update):
     print(bot.message.text)
@@ -14,10 +39,12 @@ def sms(bot, update):
     time.sleep(1)
     bot.message.reply_text("Меня зовут Кагуя Синомия. Чем могу помочь?", reply_markup=keyboard)
 
+
 def reply(bot, update):
-    replys = ["Cделаю вид, что ничего не слышала", "Чего???", "ШТООООООО??", "Ты вообще понимаешь, что говоришь, дурачье",
-               "Ну ладно)", "Эммм", "Ну ты конечно....", "АШАЛЕТЬ"]
-    rep = replys[random.randint(0, len(replys)-1)]
+    if compute_emo_rate(bot.message.text) < 0:
+        rep = NEGATIVE_REPLIES[random.randint(0, len(NEGATIVE_REPLIES) - 1)]
+    else:
+        rep = POSITIVE_REPLIES[random.randint(0, len(POSITIVE_REPLIES) - 1)]
     print(bot.message.text)
     time.sleep(1)
     bot.message.reply_text(rep)
@@ -68,16 +95,14 @@ def sendday(bot, update):
     bot.message.reply_text('Хорошего дня, {})'.format(bot.message.chat.first_name))
 
 
-
-
 def main():
+    read_words()
     bot = Updater("5260290537:AAFKwTweXBj02R7Y2Fog6nu6nU9obCIEnn8", use_context=True)
     bot.dispatcher.add_handler(CommandHandler('start', sms))
     bot.dispatcher.add_handler(MessageHandler(Filters.regex('Кто я сегодня?'), whoami))
     bot.dispatcher.add_handler(MessageHandler(Filters.regex('Скинь ножки'), sendlegs))
     bot.dispatcher.add_handler(MessageHandler(Filters.regex('Какой сегодня день?'), sendday))
     bot.dispatcher.add_handler(MessageHandler(Filters.text, reply))
-
 
     bot.start_polling()
     bot.idle()
