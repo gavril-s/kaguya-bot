e205dfa925be641fd30f954adb22bfea73064b16
aJlax <chernykh-2004@yandex.ru>
2022-04-07 21:47:37 +0300

    prodakwn

--- scripti4.py
+++ scripti4.py
@@ -359,13 +359,10 @@ def reply(bot, update): # ответ на обычное сообщение
     log(bot.message)
     if bot.message.text == QUIT_TEXT and usr_id in ADMINS_ID:
         quit()
-    if 'или' in bot.message.text:
-        rep = bot.message.text.split('или ')
-        if rep[0] != '' and rep[0] != ' ':
-            time.sleep(SLEEP_TIME)
-            bot.message.reply_text(OR_ANSWERS[random.randint(0, len(OR_ANSWERS) - 1)], rep[random.randint(0, 1)])
-
-    if random.random() <= 0.01: # имба, редкость
+    if 'или' in bot.message.text and bot.message.text.split('или ')[0] != ' ' and bot.message.text.split('или ')[0] != '':
+        time.sleep(SLEEP_TIME)
+        bot.message.reply_text(OR_ANSWERS[random.randint(0, len(OR_ANSWERS) - 1)], bot.message.text.split('или ')[random.randint(0, 1)])
+    elif random.random() <= 0.01: # имба, редкость
         bot.message.reply_text('Когда ты мне пишешь...')
         time.sleep(SLEEP_TIME)
         bot.message.reply_text('Твоё сообщение')
